65273de990a309b571a09e0f5c9faaf3ac8d2245
Baptiste Szmoniewski <b.szmoniewski@orange.fr>
2024-01-16 15:25:53 +0100

    rajout de la boucle for afin de sauter de ligne

--- terre02-sh.sh
+++ terre02-sh.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 function File {
-    echo "$@"
+    for arg in "$@"; do
+        echo "$arg"
+    done
 }
-
 
 if [ ! $# -lt 1 ]; then
     File "$@"
     exit 0
 fi
 
-
-bash terre02-sh.sh Shell is fun
+bash terre02-sh.sh je suis solide
