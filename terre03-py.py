3fe316995d67c0a5ff5e201d3586dbb23d603d94
Baptiste Szmoniewski <b.szmoniewski@orange.fr>
2024-01-16 16:03:08 +0100

    pouvoir donner sa lettre directement sur le terminal $ ./terre03-py.py ./terre03-py.py: line 1: donnee_lettre: command not found ./terre03-py.py: line 2: syntax error near unexpected token `(' ./terre03-py.py: line 2: `for i in range(ord(donnee_lettre), ord('z') + 1):'

--- terre03-py.py
+++ terre03-py.py
@@ -1,5 +1,8 @@
-donnee_lettre = "k"
-for i in range(ord(donnee_lettre), ord('z') + 1):
-    print(chr(i), end=' ')
+donnee_lettre = input("Entrer une lettre: ")
 
-print()
+if len(donnee_lettre) == 1 and donnee_lettre.isalpha():
+    for i in range(ord(donnee_lettre), ord('z') + 1):
+        print(chr(i), end=' ')
+
+    print()
+else : print("j'ai demandé qu'une lettre")
