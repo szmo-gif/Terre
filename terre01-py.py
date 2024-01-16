34e682ea2edd3b870043bb8550407c6d96044e6e
Baptiste Szmoniewski <b.szmoniewski@orange.fr>
2024-01-16 11:22:47 +0100

    supprimer le fichier python

--- terre01-py.py
+++
@@ -1,8 +0,0 @@
-import os
-
-chemin_du_fichier = "terre01-py.py"
-
-# Obtenez le nom du fichier à partir du chemin d'accès
-nom_du_fichier = os.path.basename(chemin_du_fichier)
-
-print("Nom du fichier :", nom_du_fichier)
