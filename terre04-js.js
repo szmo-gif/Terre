3ab086debb83fbf46745c98b9cf708a3c30c70ac
Baptiste Szmoniewski <b.szmoniewski@orange.fr>
2024-01-17 09:33:46 +0100

    nombre à renseigner dans le terminal

--- terre04-js.js
+++ terre04-js.js
@@ -2,11 +2,28 @@ function pair_impair(nombre) {
     if (nombre % 2 === 0) {
         return "Pair";
     } else {
-        return "Impair"
+        return "Impair";
     }
 }
 
-console.log(pair_impair(4))
-console.log(pair_impair(5))
-console.log(pair_impair(-2))
-console.log(pair_impair(-5))
+const readline = require('readline');
+const interface = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+interface.question("Entrer un chiffre : ", function(nombre) {
+
+    const resultat = pair_impair(nombre);
+
+    console.log(`${resultat}`);
+
+    interface.close();
+});
+
+
+
+//console.log(pair_impair(4))
+//console.log(pair_impair(5))
+//console.log(pair_impair(-2))
+//console.log(pair_impair(-5))
