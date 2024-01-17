e19b16fd0b084f1eb2c57f6a92d278be9b0f2e20
Baptiste Szmoniewski <b.szmoniewski@orange.fr>
2024-01-17 17:00:50 +0100

    possibilité que nombre entier

--- terre05-js.js
+++ terre05-js.js
@@ -1,8 +1,9 @@
 function division(numb1, numb2) {
-    if (isNaN(numb1) || isNaN(numb2))
+    if (isNaN(numb1) || isNaN(numb2)) {
         throw new Error("Choisissez un nombre");
+    }
 
-    if (numb1 / numb2) {
+    if (Number.isInteger(numb1 / numb2)) {
         console.log(numb1 / numb2);
     } else {
         console.log("Non divisible");
@@ -18,7 +19,7 @@ const interface = readline.createInterface({
 interface.question("Entrer un chiffre : ", function (numb1) {
     interface.question("Entrer un chiffre : ", function (numb2) {
         try {
-            division(numb1, numb2);
+            division(parseFloat(numb1), parseFloat(numb2));
         } catch (error) {
             console.error(error.message);
         }
