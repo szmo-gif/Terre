b1abd04b7a8fe0844cd28bc4f26657503dd17a59
Baptiste Szmoniewski <b.szmoniewski@orange.fr>
2024-01-17 16:46:22 +0100

    fonction pour divisé 2 chiffre

---
+++ terre05-js.js
@@ -0,0 +1,27 @@
+function division(numb1, numb2) {
+    if (isNaN(numb1) || isNaN(numb2))
+        throw new Error("Choisissez un nombre");
+
+    if (numb1 / numb2) {
+        console.log(numb1 / numb2);
+    } else {
+        console.log("Non divisible");
+    }
+}
+
+const readline = require('readline');
+const interface = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+interface.question("Entrer un chiffre : ", function (numb1) {
+    interface.question("Entrer un chiffre : ", function (numb2) {
+        try {
+            division(numb1, numb2);
+        } catch (error) {
+            console.error(error.message);
+        }
+        interface.close();
+    });
+});
