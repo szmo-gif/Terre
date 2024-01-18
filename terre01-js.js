c140ac905e89bde75d90ea7e37abe5c6a379a209
Baptiste Szmoniewski <b.szmoniewski@orange.fr>
2024-01-18 12:02:20 +0100

    changement de langage pour terre01

---
+++ terre01-js.js
@@ -0,0 +1,4 @@
+const path = require('path');
+
+const nomFichier = path.basename(__filename);
+console.log(`Le nom du fichier est : ${nomFichier}`);
--- terre01-sh.sh
+++
@@ -1,5 +0,0 @@
-chemin_du_fichier="terre01-sh.sh"
-
-nom_du_fichier=$(basename "$chemin_du_fichier")
-
-echo  $nom_du_fichier
