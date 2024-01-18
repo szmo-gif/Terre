e1364c7864905d3b3d8ca3511c23f8bdc4cd6d60
Baptiste Szmoniewski <b.szmoniewski@orange.fr>
2024-01-18 14:53:30 +0100

    changement de langue pour terre03

--- terre03-py.py
+++
@@ -1,8 +0,0 @@
-donnee_lettre = input("Entrer une lettre: ")
-
-if len(donnee_lettre) == 1 and donnee_lettre.isalpha():
-    for i in range(ord(donnee_lettre), ord('z') + 1):
-        print(chr(i), end=' ')
-
-    print()
-else : print("j'ai demandé qu'une lettre")
