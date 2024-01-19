9c1113d8287aa795dd3ebafd519a65df90b94694
Baptiste Szmoniewski <b.szmoniewski@orange.fr>
2024-01-19 15:00:28 +0100

    sans utiliser .split et .map

--- terre07-js.js
+++ terre07-js.js
@@ -1,10 +1,14 @@
 const user_string = process.argv.slice(2);
-const letters = user_string.map(argument => argument.split(''));
 
-function calculer_arguments(tableau_de_lettre) {
-    return tableau_de_lettre.reduce((acc, mot) => acc + mot.map(() => 1).reduce((acc, val) => acc + val, 0), 0);
+let nombre_arguments = 0;
+
+for (let word of user_string) {
+    for (let char of word) {
+        if (char === ' ') {
+            nombre_arguments++;
+        }
+        nombre_arguments++;
+    }
 }
-
-const nombre_arguments = calculer_arguments(letters);
 
 console.log(nombre_arguments);
