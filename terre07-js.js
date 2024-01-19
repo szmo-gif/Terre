642dc2f63287404502339c4c3e87d21506d9f489
Baptiste Szmoniewski <b.szmoniewski@orange.fr>
2024-01-19 15:22:51 +0100

    calcul bon et gestion d'erreur bonne

--- terre07-js.js
+++ terre07-js.js
@@ -1,14 +1,25 @@
-const user_string = process.argv.slice(2);
+const user_string = process.argv[2];
 
-let nombre_arguments = 0;
-
-for (let word of user_string) {
-    for (let char of word) {
-        if (char === ' ') {
-            nombre_arguments++;
-        }
-        nombre_arguments++;
+try {
+    if (!user_string) {
+        throw new Error("Erreur : aucune chaîne fournie.");
     }
-}
 
-console.log(nombre_arguments);
+    if (!isNaN(user_string)) {
+        throw new Error("Erreur : la chaîne ne doit pas contenir des chiffres.");
+    }
+
+    if (process.argv.length !== 3) {
+        throw new Error("Erreur : il doit y avoir une seule chaîne.");
+    }
+
+    let nombre_caracteres = 0;
+
+    for (let char of user_string) {
+        nombre_caracteres++;
+    }
+
+    console.log(nombre_caracteres);
+} catch (error) {
+    console.error(error.message);
+}
