ae741f789efb9f80a285b566d87b3de323bbf1ac
Baptiste Szmoniewski <b.szmoniewski@orange.fr>
2024-01-19 10:53:34 +0100

    permet de calculer chaque lettre

--- terre07-js.js
+++ terre07-js.js
@@ -1,3 +1,10 @@
 const user_string = process.argv.slice(2);
-const letter = user_string.map(argument => argument.split(''));
-console.log(letter)
+const letters = user_string.map(argument => argument.split(''));
+
+function calculer_arguments(tableau_de_lettre) {
+    return tableau_de_lettre.reduce((acc, mot) => acc + mot.map(() => 1).reduce((acc, val) => acc + val, 0), 0);
+}
+
+const nombre_arguments = calculer_arguments(letters);
+
+console.log(nombre_arguments);
